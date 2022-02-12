28861211a3f2e4dfb045d44f69849157944fc401
rahul3355 <rahulpatil.stallion@gmail.com>
2022-02-12 17:49:06 +0530

    second commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://rahulpatil:berlin2000@cluster0.paf1n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//mongodb+srv://rahulpatil:berlin2000@cluster0.paf1n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+//'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+//mongoose.set('useFindAndModify', false);
